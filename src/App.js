21d6b63600f3db5f336e09a67bbc6b17e4993837
Hoolan01 <hoolan01@gmail.com>
2021-08-30 12:24:34 +0400

    title change

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-// function App() {
-//     return (
-//         <div className="App">
-//             <header className="App-header">
-//                 <img src={logo} className="App-logo" alt="logo" />
-//                 <p>My name is Hoolan</p>
-//                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-//                     Learn React
-//                 </a>
-//             </header>
-//         </div>
-//     );
-// }
-
 class App extends Component {
   constructor() {
     super();
@@ -41,9 +27,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1>
-          Pussy <s>Cat</s> Lovers...
-        </h1>
+        <h1>Find a Cat...</h1>
         <SearchBox placeholder="Search Cat" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
